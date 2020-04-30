07f16aea521461a7150a0561ff8191f2596b9b34
Luciano <lucianoaf8@gmail.com>
2020-04-30 09:19:57 -0600

    Update gitignore

--- app.js
+++ app.js
@@ -7,7 +7,9 @@ const https = require("https");
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
 app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
@@ -19,16 +21,14 @@ app.post("/", function (req, res) {
     const email = req.body.email;
 
     const data = {
-        members: [
-            {
-                email_address: email,
-                status: "subscribed",
-                merge_fields: {
-                    FNAME: firstName,
-                    LNAME: lastName,
-                },
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName,
             },
-        ],
+        }, ],
     };
 
     const jsonData = JSON.stringify(data);
@@ -60,6 +60,6 @@ app.post("/failure", function (req, res) {
     res.redirect("/");
 });
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, function () {
     console.log("Server is running on port 3000");
 });
